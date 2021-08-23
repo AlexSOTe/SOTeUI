b03db8f20ba457ece31b9ac690cf81e37590f1c9
AlexLiu <alexsote@outlook.com>
2021-08-23 12:00:48 +0800

    删除无用文件

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
+// @ts-ignore
 import { defineConfig } from 'vite'
+// @ts-ignore
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
@@ -6,6 +8,6 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
-    port: 3000
+    port: 4200
   }
 })
