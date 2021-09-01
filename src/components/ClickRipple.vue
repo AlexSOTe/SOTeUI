c441d66dc3f2f2f8019994a2e6000b70b24a80fc
AlexLiu <alexsote@outlook.com>
2021-09-01 14:39:56 +0800

    优化ripple的鼠标坐标获取

--- src/components/ClickRipple.vue
+++ src/components/ClickRipple.vue
@@ -6,24 +6,6 @@
 </script>
 <style scoped lang="scss">
   .ClickRipple {
-  }
-</style>
-<style>
-  /*全局css*/
-  @keyframes RippleSpread {
-    from {
-      transform: scale(1);
-      opacity: 1;
-    }
-
-    to {
-      transform: scale(6);
-      opacity: 0;
-    }
-  }
-
-  .ripple-animate {
-    animation: RippleSpread 300ms ease-out;
-    animation-fill-mode: forwards;
+    height: 450px;
   }
 </style>
--- src/directives/ripple.ts
+++ src/directives/ripple.ts
@@ -25,17 +25,16 @@ export class RippleDirective {
   SetEleStyle(ele: HTMLElement) {
     ele.setAttribute('style', `
 position:relative;
-height: 450px;
 border: 1px solid #369;
 user-select: none;
 `)
   }
   DealEvent(ele: HTMLElement) {
     ele.addEventListener('mousedown', (evt: MouseEvent) => {
-      console.log(evt.offsetX, evt.offsetY);
+      console.log(evt);
       // 获取点击时鼠标在元素中的位置
-      const mx = evt.offsetX;
-      const my = evt.offsetY;
+      const mx = evt.clientX - ele.offsetLeft;
+      const my = evt.clientY - ele.offsetTop;
       // 创建一个元素用来包含点击动画所需的元素
       this.aniBox = this.CreateAniBox(ele, new Vector2(mx, my));
       ele.appendChild(this.aniBox);
@@ -44,9 +43,6 @@ user-select: none;
         // 先这样移除吧，真正移除需要对内存做处理，会用到WeakMap，再说，反正我这不会内存溢出，毕竟OvO 32G内存
         this.aniBox?.remove();
       }, 300);
-    });
-    ele.addEventListener('mouseup', (evt: MouseEvent) => {
-      this.aniBox?.remove();
     });
   }
   CreateAniBox(ele: HTMLElement, pos: Vector2): HTMLElement {
@@ -64,6 +60,21 @@ height:0;
 box-shadow: 0 0 10px 5px hsl(${colorNum},50%,50%);
 transform:translate(-50%,-50%);
 border-radius:50%;
+animation: RippleSpread 300ms ease-out;
+animation-fill-mode: forwards;
+`);
+    document.styleSheets.item(0)?.insertRule(`
+@keyframes RippleSpread {
+  from {
+    transform: scale(1);
+    opacity: 1;
+  }
+
+  to {
+    transform: scale(6);
+    opacity: 0;
+  }
+}
 `);
     return aniBox;
   }
